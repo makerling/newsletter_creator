cc05ab93d46d4c803261cf6d135c4b0ef06b8c5b
Stevan Vanderwerf <github@vanderling.com>
2020-09-22 12:21:51 -0500

    reworked onOpen function for brevity

--- Code.js
+++ Code.js
@@ -1,68 +1,24 @@
-//FOR NEW Account need to do following:
-//enable > Resources/Advanced Google Services > Gmail API and Drive API (also check on link to make sure they are activated in Google Cloud Platform API Dashboard)
-/** Build a menu item
-From https://developers.google.com/apps-script/guides/menus#menus_for_add-ons_in_google_docs_or_sheets 
-ToDo:
-- check names with communicator list 
-- catch inline pics
-- change green color to real green, remove alternative colors in rows
-**/
-
+// FOR NEW Account need to do following:
+// enable > Resources/Advanced Google Services > Gmail API and Drive API 
+// (also check to make sure they are activated in Google Cloud Platform API Dashboard)
 //******************************************************************************************************
 
+// adds menu items in spreadsheet when opened
 function onOpen(e) {
-  var menu = SpreadsheetApp.getUi().createMenu('communicator'); 
+  
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var menuEntries = [];
+  // When the user clicks on "addMenuExample" then "Menu Entry 1", the function function1 is executed.
+  menuEntries.push({name: "1 Insert Email text", functionName: "pullingEmails"});
+  menuEntries.push(null); // line separator
+  menuEntries.push({name: "2 Send as Word doc my Email", functionName: "sendingEmails"});
 
-var menu1 = "'1 Insert Email text', 'pullingEmails'"
-var menu2 = "'2 Send as Word doc my Email', 'sendingEmails'"
-
-if (e && e.authMode == ScriptApp.AuthMode.NONE) {
-    // Add a normal menu item (works in all authorization modes).
-        menu.addItem(menu1);  
-        menu.addItem(menu2);                
-  } else {
-    // Add a menu item based on properties (doesn't work in AuthMode.NONE).
-    var properties = PropertiesService.getDocumentProperties();
-    var workflowStarted = properties.getProperty('workflowStarted');
-    if (workflowStarted) {
-        menu.addItem(menu1);  
-        menu.addItem(menu2);    
-    } else {
-        menu.addItem(menu1);  
-        menu.addItem(menu2);   
-  }
-   menu.addToUi();
-  }
+  ss.addMenu("Newsletter", menuEntries);
 }
 
-//******************************************************************************************************
-
-//need to change manually to picsFolderId value
-//only the owner of the file may trash or delete the file
-//function DeleteOldFiles() {
-//  var Folders = new Array(
-//    ''
-//  );
-//  var Files;
-
-//  for each (var FolderID in Folders) {
-//    Folder = DriveApp.getFolderById(FolderID)
-//    Files = Folder.getFiles();
-
-//    while (Files.hasNext()) {
-//      var File = Files.next();
-
-//      File.setTrashed(true); // Places the file int the Trash folder
-      //Drive.Files.remove(File.getId()); // Permanently deletes the file
-//      Logger.log('File ' + File.getName() + ' was deleted.');
-//    }
-//  }
-//}
-
-//******************************************************************************************************
+//***********************************************************************************
 
 function pullingEmails(){
-  //DeleteOldFiles();
   emailToSpreadsheet();
   replace();
   sort();
--- replace.js
+++ replace.js
@@ -1,35 +1,26 @@
-//this array variable stores all the members of the entity with the following information: email address, Team, first name, last name, first name(s) of household unit
-//first names of household unit are used because members didn't want their last name put on the newsletter, but last name is needed internally for sorting the entries
-var name = new Array ( );
-  //name[0] = new Array ('name@email.com','team name','first name','last name','first & last name of couple');
- name[0] = new Array ('optimizer@vanderling.com','T1','Joe','Bradson','Joe & Donna Bradson');
- name[1] = new Array ('voip.ms@vanderling.com','T2','Jack','Greg','Jack & Sarah Greg');
- name[2] = new Array ('zipcar@vanderling.com','T3','Brad','Jackson','Brad & Darlene Jackson');
-  
-//This function populates the "editColumn" function, it will run 3 times with the 3 values in this function, once for each column that needs changing in the template
-function replace(){
-  editColumn('2','4'); //FIRST NAMES: template column number (starting with "1), array item (starting with "0");
-  Logger.log('**********************************');
-  editColumn('3','3'); //LAST NAME: template column number (starting with "1), array item (starting with "0");
-  Logger.log('**********************************');
-  editColumn('4','1'); //TEAM: template column number (starting with "1), array item (starting with "0");
-}
-
 function editColumn(templateColumnNum,arrayItem) {
-  var names = new Array (name);
 
   var ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(active_sheet);
   var lrow = ss.getLastRow();
+
+  var ss2 = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(data);
+  var lrow2 = ss2.getLastRow();
   
   // Set the scope of the array from the Spreadsheet
   var rngFirstNameCouple = ss.getRange(2, templateColumnNum, lrow - 1, 1);
   var dataFirstNameCouple = rngFirstNameCouple.getValues();
+
+  // row, column, numRows, numColumns
+  var data2 = ss2.getRange(1,templateColumnNum, lrow2 -1, 1)
+  var data2values = data2.getValues();
   
   // Code to convert email address to desired value for 2nd, 3rd and 4th column of template
   // iterate through the list of names in template, and match email address in array and populate the columns with corresponding data from array
   for (var d=0; d < dataFirstNameCouple.length; d++) {
-    for ( i=0; i<name.length; i++ ) {
-      for ( j=0; j<name[i][0].length; j++ ) {
+    //for ( i=0; i<name.length; i++ ) {
+    for ( i=0; i<data2values.length; i++ ) {
+      // for ( j=0; j<name[i][0].length; j++ ) {
+      for ( j=0; j<data2values[i][0].length; j++ ) {
         //if the email address (first item) in template matches the email address of the array (0 = first item of matched array), replace the email address in column to desired result (last name, team, etc)
         if (name[i][0] === dataFirstNameCouple[d][0]) {
         dataFirstNameCouple[d][0] = name[i][arrayItem];
@@ -40,4 +31,13 @@ function editColumn(templateColumnNum,arrayItem) {
     }
   }
   rngFirstNameCouple.setValues(dataFirstNameCouple);
+}
+
+// 3 times with the 3 values in this function, once for each column that needs changing in the template
+function replace(){
+  editColumn('2','4'); //FIRST NAMES: template column number (starting with "1), array item (starting with "0");
+  Logger.log('**********************************');
+  editColumn('3','3'); //LAST NAME: template column number (starting with "1), array item (starting with "0");
+  Logger.log('**********************************');
+  editColumn('4','1'); //TEAM: template column number (starting with "1), array item (starting with "0");
 }
